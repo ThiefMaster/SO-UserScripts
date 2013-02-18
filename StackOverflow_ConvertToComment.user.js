20287c367b9057a8ac09c16d87a99c5b5fb0bd6d
Adrian <adrian@planetcoding.net>
2013-02-18 14:23:22 +0100

    add convert-to-comment script

---
+++ StackOverflow_ConvertToComment.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name StackOverflow convert to comment
+// @namespace adrian@planetcoding.net
+// @description Enable convert-to-comment even if there are comments
+// @include http://stackoverflow.com/admin/dashboard*
+// @version 1.0
+// @grant none
+// @run-at document-start
+// ==/UserScript==
+
+var triggered = false;
+document.addEventListener('readystatechange', function() {
+    if(document.readyState != 'uninitialized' && document.readyState != 'loading' && !triggered) {
+        triggered = true;
+        [].forEach.call(document.querySelectorAll('input[disabled][value="convert to comment"]'), function(elem) {
+            elem.disabled = false;
+            elem.classList.add('convert-to-comment');
+            elem.style.boxShadow = '0px 0px 5px rgba(255, 0, 0, 1), 0 1px 0 rgba(255, 255, 255, 0.4) inset';
+        });
+    }
+}, true);
+
+var confirm = window.confirm;
+window.confirm = function(message) {
+    if(message == 'The answer will become a comment on the question; are you sure?') {
+        return true;
+    }
+    return confirm.apply(this, arguments);
+};
