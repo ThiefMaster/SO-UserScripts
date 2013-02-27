9d1296e07ad3bf2223dacf191934698f3e87cd96
Adrian <adrian@planetcoding.net>
2013-02-27 13:28:38 +0100

    Remove stuff that is now natively available on SO

--- StackOverflow_ConvertToComment.user.js
+++ StackOverflow_ConvertToComment.user.js
@@ -1,25 +1,11 @@
 // ==UserScript==
 // @name StackOverflow convert to comment
 // @namespace adrian@planetcoding.net
-// @description Enable convert-to-comment even if there are comments
+// @description Remove the confirmation prompt when converting an answer to a comment
 // @include http://stackoverflow.com/admin/dashboard*
-// @version 1.0
+// @version 1.1
 // @grant none
-// @run-at document-start
 // ==/UserScript==
-
-var triggered = false;
-document.addEventListener('readystatechange', function() {
-    if(document.readyState != 'uninitialized' && document.readyState != 'loading' && !triggered) {
-        triggered = true;
-        [].forEach.call(document.querySelectorAll('input[disabled][value="convert to comment"]'), function(elem) {
-            elem.disabled = false;
-            elem.classList.add('convert-to-comment');
-            elem.style.boxShadow = '0px 0px 5px rgba(255, 0, 0, 1), 0 1px 0 rgba(255, 255, 255, 0.4) inset';
-            elem.title = 'make this answer a comment on the original question (ignores comments on the answer)';
-        });
-    }
-}, true);
 
 var confirm = window.confirm;
 window.confirm = function(message) {
